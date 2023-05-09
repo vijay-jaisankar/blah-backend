af64ab2c5bd1337d7874a15dd8ed968c72cf17a4
vijay-jaisankar <vijayjaisankar.vj@gmail.com>
2023-05-09 22:55:10 +0530

    bugfix: appropriate response codes

--- details-controller/main.py
+++ details-controller/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status
+
+
 from typing import Optional
 import os
 from dotenv import load_dotenv
@@ -24,22 +26,24 @@ def get_status():
     Get details about a particular entry with IMDB ID
 """
 @app.get("/details/{item_id}")
-def get_details(item_id: str, api_key: str = OMDB_API_KEY) -> Optional[dict]:
+def get_details(item_id: str, response: Response, api_key: str = OMDB_API_KEY) -> Optional[dict]:
     # Call the OMDB API
     r = requests.get(f"http://www.omdbapi.com/?i={item_id}&apikey={api_key}")
 
     # Base case
     if r.status_code != 200:
+        response.status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
         return {
-            "error_message": "Server error or invalid ID passed"
+            "error_message": "Server error"
         }
     
     # Get attributes
     json_description = r.json()
 
     if 'Error' in json_description:
+        response.status_code = status.HTTP_404_NOT_FOUND
         return {
-            "error_message": "Server error or invalid ID passed"
+            "error_message": "Invalid ID passed"
         } 
     
     if 'Title' in json_description:

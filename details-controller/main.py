06bfd4c7b5acd88374b35d09fff332fb3d4f0190
vijay-jaisankar <vijayjaisankar.vj@gmail.com>
2023-05-09 22:49:43 +0530

    feat: get details route

--- details-controller/main.py
+++ details-controller/main.py
@@ -1,4 +1,14 @@
 from fastapi import FastAPI
+from typing import Optional
+import os
+from dotenv import load_dotenv
+import requests
+
+# Environment variables
+load_dotenv()
+OMDB_API_KEY = os.getenv('OMDB_API_KEY')
+
+
 
 app = FastAPI()
 
@@ -10,4 +20,40 @@ app = FastAPI()
 def get_status():
     return {"status": "up ✓"}
 
+"""
+    Get details about a particular entry with IMDB ID
+"""
+@app.get("/details/{item_id}")
+def get_details(item_id: str, api_key: str = OMDB_API_KEY) -> Optional[dict]:
+    # Call the OMDB API
+    r = requests.get(f"http://www.omdbapi.com/?i={item_id}&apikey={api_key}")
 
+    # Base case
+    if r.status_code != 200:
+        return {
+            "error_message": "Server error or invalid ID passed"
+        }
+    
+    # Get attributes
+    json_description = r.json()
+
+    if 'Error' in json_description:
+        return {
+            "error_message": "Server error or invalid ID passed"
+        } 
+    
+    if 'Title' in json_description:
+        title = json_description['Title']
+    else:
+        title = "Untitled"
+    if 'Poster' in json_description:
+        poster = json_description['Poster']
+    else:
+        poster = "https://i.imgur.com/oA0yD7d.png" # Default image
+
+    # Return JSON
+    return {
+        "id": item_id,
+        "title": title,
+        "poster" : poster
+    }

9307b420790d90a32066a79d89321d3f094215f3
vijay-jaisankar <vijayjaisankar.vj@gmail.com>
2023-05-13 19:08:39 +0530

    feat: mindsdb sdk setup

---
+++ sentiment-controller/index.js
@@ -0,0 +1,90 @@
+/*
+Imports
+*/
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const MindsDB = require("mindsdb-js-sdk");
+
+
+// MindsDB setup
+const user = {
+    user: process.env.MINDSDB_USER,
+    password: process.env.MINDSDB_PASS,
+};
+
+const connectToMindsDB = async (user) => {
+    await MindsDB.default.connect(user);
+    console.log("Connected to MindsDB!");
+};
+
+const getSentimentOfText = async (text) => {
+    const model = await MindsDB.default.Models.getModel(
+        "sentiment",
+        "mindsdb"
+    );
+
+    const queryOptions = {
+        where: [`comment = "${text}"`],
+    };
+
+    const prediction = await model.query(queryOptions);
+    return prediction;
+};
+
+// Express API setup
+const app = express();
+app.use(cors(({
+    origin: "*"
+})));
+
+app.use(
+    bodyParser.urlencoded({
+        extended: false,
+    })
+);
+app.use(bodyParser.json());
+
+app.use(function (req, res, next) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+        "Access-Control-Allow-Methods",
+        "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+    );
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Access-Control-Allow-Origin: *",
+        "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization"
+    );
+    next();
+});
+
+
+
+// Base route
+app.get("/", function (req, res) {
+    return res.json("Hello world!");
+});
+
+// Sentiment analysis route
+app.post("/sentiment", async function (req, res) {
+    let text = req.body.text;
+    try {
+        await connectToMindsDB(user);
+        let summaryText = await getSentimentOfText(text);
+        let retValue = summaryText["data"]["PRED"];
+        res.json({ summary: retValue });
+    } catch (error) {
+        console.log(`Error: ${error}`);
+        res.json(error);
+    }
+});
+
+// Run the API
+const port = 9000;
+app.listen(port, () => {
+    console.log(`Listening at Port ${port}`);
+});
+
+module.exports = app;
